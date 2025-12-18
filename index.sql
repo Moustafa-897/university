3194a2975098682b0e354571b44010640226f290
moustafa <alsayedmoustafa101997@gmail.com>
2025-12-18 18:26:59 +0200

    index v5

--- index.sql
+++ index.sql
@@ -5,3 +5,4 @@ CREATE TABLE departments(
 CREATE TABLE courses( course_id INT PRIMARY KEY, course_name VARCHAR(50) NOT NULL, credits INT NOT NULL DEFAULT 3, dept_id INT, FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE CASCADE );   
 CREATE TABLE students( student_id INT PRIMARY KEY, student_name VARCHAR(50) NOT NULL );
 CREATE TABLE enrollments ( enrollment_id INT PRIMARY KEY, student_id INT, course_id INT, CONSTRAINT fk_enrollments_student FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE, CONSTRAINT fk_enrollments_course FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE );
+CREATE TABLE health_records ( record_id INT PRIMARY KEY, student_id INT, blood_group TEXT, CONSTRAINT fk_health_records_student FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE );
