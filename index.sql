c23c4085ee410694ccb5e1a01784781cd4f4b3fb
moustafa <alsayedmoustafa101997@gmail.com>
2025-12-18 18:02:38 +0200

    index v3

--- index.sql
+++ index.sql
@@ -3,3 +3,4 @@ CREATE TABLE departments(
     dept_id INT PRIMARY KEY, 
     dept_name VARCHAR(50) NOT NULL );
 CREATE TABLE courses( course_id INT PRIMARY KEY, course_name VARCHAR(50) NOT NULL, credits INT NOT NULL DEFAULT 3, dept_id INT, FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE CASCADE );   
+CREATE TABLE students( student_id INT PRIMARY KEY, student_name VARCHAR(50) NOT NULL );
